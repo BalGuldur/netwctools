4fea3e58c121ba43fc88404c69b7f09a40464740
Aleksandr Krulov <a.krulov@google.com>
2011-12-12 15:59:15 +0400

    First commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Netwctools::Application.config.secret_token = '6ea3817d53839137b98dbce0e79fef115ce68510c400e43b82d72243eaf1723480a7404844f9f08bccdc5f86806bf06535c6fef62e82a165653d494cd1fdb7ec'
