8ca85cd8369f4238f3ffaf7c0011c4198d3bf369
Aleksandr Krulov <a.krulov@gmail.com>
2012-04-12 11:24:35 +0400

    before begin check db scheme

--- app/models/domain.rb
+++ app/models/domain.rb
@@ -1,6 +1,6 @@
 class Domain < ActiveRecord::Base
 
-	belongs_to :myuser
+	belongs_to :myuser #correct this to has_and_belongs_to_many
 	has_many :vlans
 	has_many :terminate_points 
 end
--- app/models/myuser.rb
+++ app/models/myuser.rb
@@ -1,3 +1,3 @@
 class Myuser < ActiveRecord::Base
-	has_many :domains
+	has_many :domains #correct this to has_and_belongs_to_many
 end
